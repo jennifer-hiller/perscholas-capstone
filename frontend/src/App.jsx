d9d255146dca4a359bc18c0d620e5f25675eb303
Jennifer Hiller <jennhi@gmail.com>
2024-11-18 13:16:15 -0800

    added external API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Detail from "./pages/Detail";
 import TaskForm from "./pages/TaskForm";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Users from "./pages/Users";
 import User from "./pages/User";
+import axios from "axios";
 function App() {
   const [user, setUser] = useState(localStorage.getItem("user"));
+  const [joke, setJoke] = useState(null);
   function logoutUser() {
     localStorage.removeItem("user");
     setUser(null);
@@ -18,8 +20,16 @@ function App() {
     localStorage.setItem("user", userData);
     setUser(userData);
   }
+  useEffect(() => {
+    async function getJoke() {
+      const data = await axios.get("https://api.chucknorris.io/jokes/random");
+      setJoke(data.data.value);
+    }
+    getJoke();
+  }, []);
   return (
     <>
+      <h6>{joke}</h6>
       <nav>
         <ul>
           <li>
